a0f82922bbee3d62b991a3016a05c51cc975c9b9
dusthinju <wustls14@naver.com>
2017-08-01 11:37:32 +0900

    2번째 수정

--- app/src/main/java/com/example/tacademy/testgit/MainActivity.java
+++ app/src/main/java/com/example/tacademy/testgit/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // 20170801 작업시작.....
+        // 20170801 작업시작..
     }
 }
